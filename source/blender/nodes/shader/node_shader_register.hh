3060ffe51d04e456ba23fe0ad8bd2ead2704b050
sean <sean@starcapgames.net>
2025-06-09 14:58:51 -0400

    TEST_PUSH01

--- source/blender/nodes/shader/node_shader_register.hh
+++ source/blender/nodes/shader/node_shader_register.hh
@@ -111,3 +111,5 @@ void register_node_type_sh_volume_principled();
 void register_node_type_sh_volume_scatter();
 void register_node_type_sh_wavelength();
 void register_node_type_sh_wireframe();
+
+/*LALALALLA THIS IS A TEST TO CHECK MY GIT REPO!!!!!!!!!!!*/
